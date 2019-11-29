0db44745ab9d807bb4a4eafd29a8c91b6aa857d8
HeeWon Lee <ncross42@gmail.com>
2019-11-29 22:04:05 +0900

    user must has only one insurance

--- server_express/routes/insure.js
+++ server_express/routes/insure.js
@@ -23,14 +23,22 @@ router.get('/contract/:user_id/:insure_id/:ts?', async (req, res, next) => {
   if ( !req.params.user_id /* === undefined */) {
     return res.status(500).json({message:'no user_id'})
   }
-
+  const user_id = parseInt(req.params.user_id)
   try {
     const db = await dbKick()
-    const ts = (!req.params.ts) ? "datetime('now', 'localtime')" : `datetime(${req.params.ts}, 'unixepoch')`
-    sql = `INSERT INTO track (user_id, ts, insure_id) VALUES ( ${req.params.user_id}, ${ts}, ${req.params.insure_id} )`
-    console.log(sql)
-    const ret = await db.run(sql)
-    res.json({track_id:ret.stmt.lastID})
+    let track_id = 0
+    // check exist contract
+    const row = await db.get(`SELECT id FROM track WHERE user_id = ${user_id} AND insure_id <> 0 LIMIT 1`);
+    if (!row) {
+      const ts = (!req.params.ts) ? "datetime('now', 'localtime')" : `datetime(${req.params.ts}, 'unixepoch')`
+      sql = `INSERT INTO track (user_id, ts, insure_id) VALUES ( ${user_id}, ${ts}, ${req.params.insure_id} )`
+      console.log(sql)
+      const ret = await db.run(sql)
+      track_id = ret.stmt.lastID
+    } else {
+      track_id = row.id
+    }
+    res.json({track_id})
   } catch (error) {
     const ret = { code:error.code, errno: error.errno, message: error.message }
     console.log(ret)
